ada24fcd35514bf8b2c73479f6cc41eb499ff1ab
Rajat Gupta <rajatx185@gmail.com>
2025-10-08 16:13:14 +0530

    Learning WaitGroup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rajatx185/golang-scalable-background-job-system
+
+go 1.25.2
---
+++ goroutines.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+	// "runtime"
+)
+
+func worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Worker %d starting\n", id)
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+	// fmt.Printf("Alive goroutines %v\n", runtime.NumGoroutine())
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 1; i <= 10; i++ {
+		wg.Add(1)         // <-- Add BEFORE launching goroutine
+		go worker(i, &wg) // // pass pointer to WaitGroup
+	}
+
+	fmt.Println("All workers launched")
+	time.Sleep(2 * time.Second)
+	wg.Wait() // wait for all goroutines to finish
+	fmt.Println("Main Exiting")
+}
